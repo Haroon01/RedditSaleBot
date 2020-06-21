d5243d00ccdfeb1e291aaf2a5b02d0b7d31262e0
haroon01 <haroon815@gmail.com>
2020-06-21 20:20:05 +0100

    first version of bot working

--- Main.py
+++ Main.py
@@ -1,33 +1,50 @@
 import praw
+import time
 
-#SaleNotifyBot
-#CoolFancyBot53!
+# SaleNotifyBot
+# CoolFancyBot53!
+# the above are details for the account "SaleNotifyBot" which is used to PM you on reddit.
 
-reddit = praw.Reddit(client_id='cDON-kXRD8QG5g',
-                     client_secret='8zNEZKBsRX3bSNDh31Z8k-f21Fs',
+# you should be able to use your own account for this too but i was unable to test this because my account
+# has 2fa enabled and it requires more work to get it working (probably an easy task but oh well lol)
+
+reddit = praw.Reddit(client_id='CuluidDV9j47mA',
+                     client_secret='_fIRuRlLs5Xd4M5_5XT61DjLO88',
                      username="SaleNotifyBot",
                      password="CoolFancyBot53!",
-                     user_agent='msg bot by u/xd-Drewski13')
+                     user_agent='msg bot by u/xd-Drewski13')  # and the reddit police?? hehehehehehe
 
 
 def scansub(sub):
-
-    keywords = ["weed", "people"]
-
+    # --------- Add your keywords you would like to track here! ---------------
+    keywords = ["2080ti", "2070", "intel"]
+    # -------------------------------------------------------------------------
     for submission in reddit.subreddit(sub).stream.submissions():
-        # print(submission.title)
-        # print(submission.author)
-        author = submission.author
         rawtitle = submission.title
         title = rawtitle.split()
         flair = submission.link_flair_text
+        link = submission.permalink
+        time.sleep(1)
         if set(keywords) & set(title):
-            print(rawtitle)
+            matchingword = set(keywords) & set(title)
+            reddit.redditor('xd-Drewski13').message(f'Match found for a {flair}!',
+                                                          f'I have found a post containing your keyowrd(s): {matchingword}\n\n'
+                                                          f'Title: {rawtitle}\n\nLink: {link}')
+            print(f"* Match Found! Sent a PM! ({rawtitle})")
         else:
             pass
 
 
-while True:
+print(f"Successfully logged in as: {reddit.user.me()}\n----------------------------------------------------")
+time.sleep(5)
+print("Bot is now running!")
+time.sleep(1)
 
-    scansub("askreddit")
-    scansub("pics")
+while True:
+    # ------- Write down the subreddits you want to track using the bot ---------------
+    # E.g. i want to track r/askreddit and r/pics:
+    #    scansub("askreddit")
+    #    scansub("pics")
+
+    scansub("hardwareswap")
+    scansub("buildapcsales")
