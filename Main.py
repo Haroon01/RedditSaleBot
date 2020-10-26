faddb2eda6333bdd95193b3e0ac4da0cb843d51b
haroon01 <haroon815@gmail.com>
2020-10-26 16:19:16 +0000

    removed sensitive info

--- Main.py
+++ Main.py
@@ -1,18 +1,11 @@
 import praw
 import time
 
-# SaleNotifyBot
-# CoolFancyBot53!
-# the above are details for the account "SaleNotifyBot" which is used to PM you on reddit.
-
-# you should be able to use your own account for this too but i was unable to test this because my account
-# has 2fa enabled and it requires more work to get it working (probably an easy task but oh well lol)
-
-reddit = praw.Reddit(client_id='CuluidDV9j47mA',
-                     client_secret='_fIRuRlLs5Xd4M5_5XT61DjLO88',
-                     username="SaleNotifyBot",
-                     password="CoolFancyBot53!",
-                     user_agent='msg bot by u/xd-Drewski13')  # and the reddit police?? hehehehehehe
+reddit = praw.Reddit(client_id='',
+                     client_secret='',
+                     username="",
+                     password="",
+                     user_agent='')
 
 
 def scansub(sub):
